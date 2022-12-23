7decece5d89a268c60a6659c929026fbc9b957b7
Javier Gomez <javiergogar12@gmail.com>
2022-12-23 12:32:32 +0100

    Video 7

--- src/app/customers/customers.page.ts
+++ src/app/customers/customers.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-customers',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomersPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  goToHome(){ //funcion para el routeo de customers->home, en vez de usar el router link, se llama a esto desde el html,se hace con la libreria router de angular
+    this.router.navigate(['/home'])
   }
 
 }
